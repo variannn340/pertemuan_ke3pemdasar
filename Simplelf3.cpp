57c39b9e73da6fa7fdeca97df988b81213352ef4
variannn340 <varianvarian0301@gmail.com>
2026-03-31 12:33:24 +0700

    tambah sebelum if

--- Simplelf3.cpp
+++ Simplelf3.cpp
@@ -7,6 +7,8 @@ int main(){
     srand(time(0));
     x = rand() % 10;
 
+cout << "Bilangan awal = "<<x<<endl;  // tambah sebelum if
+
     if (x==5){
         x = x+1;
         cout<<"nilai x sudah dikoreksi menjadi : "<<x<<endl;
